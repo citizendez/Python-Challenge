07fd52fc98d7fd28c8a387f0ce3bc31e56bb5191
Desiree Herschberger <citizendez@gmail.com>
2020-10-14 11:54:46 -0400

    update python case file

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -8,6 +8,7 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+csvoutput = os.path.join ('Analysis', 'FinancialAnalysis.txt')
 #print(csvpath)
 
 #  open csv file with reader
@@ -31,8 +32,6 @@ with open(csvpath) as csvfile:
         counter = counter + 1 #row counter of months
         profit_loss = float(row[1])
         total_PL = total_PL + profit_loss #total profit/loss
-    #print(total_PL)
-    #print(counter)
     
     #The greatest increase in profits (date and amount) over the entire period
         diff = profit_loss - prev 
@@ -40,29 +39,25 @@ with open(csvpath) as csvfile:
         if diff > max_diff:
             max_diff = diff
             max_date = row[0]
-    #print(max_diff)
-    #print(max_date)
     
     #The greatest decrease in losses (date and amount) over the entire period
         if diff < low_diff:
             low_diff = diff
             low_date = row[0]
             
-            #The average of the changes in "Profit/Losses" over the entire period
-        if counter > 1:   #The average of the changes in "Profit/Losses" over the entire period
+    #   
+        if counter > 1:    
             total_diff += diff
-    #print(low_diff)
-    #print(low_date)
-
-    #output for text file
+            
+    #The average of the changes in "Profit/Losses" over the entire period
     average_diff = round(total_diff/(counter - 1), 2)
-    #print(average_diff)
 
+ #output for text file
     #string variables
     header = 'Financial Analysis\n'
     divider = '----------------------------\n'
     total = 'Total: $%.0f \n'%total_PL
-    ttlMonths = f'Total Months {counter}\n'
+    ttlMonths = f'Total Months: {counter}\n'
     avgChange = f'Average Change: ${average_diff}\n'
     grtIncrease = f'Greatest Increase in Profits: {max_date} $%.0f \n'%max_diff
     grtDecrease = f'Greatest Decrease in Profits: {low_date} $%.0f \n'%low_diff
@@ -70,5 +65,5 @@ with open(csvpath) as csvfile:
     Output = header + divider + ttlMonths + total + avgChange + grtIncrease + grtDecrease
     print(Output)
     
-with open("Analysis/FinancialAnalysis.txt", "w") as f:
-    f.write(Output)
+with open(csvoutput, "w") as txt_file:
+    txt_file.write(Output)
